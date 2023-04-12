fce8ac7210066ffb157f1cfb5f1240a71ae84c5e
Damian Kuriata <damianisko7.dk@gmail.com>
2023-04-12 20:02:05 +0100

    Rename ngx-http-cache to http-cache

--- projects/http-cache/karma.conf.js
+++ projects/http-cache/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/ngx-http-cache'),
+      dir: require('path').join(__dirname, '../../coverage/http-cache'),
       subdir: '.',
       reporters: [
         { type: 'html' },
