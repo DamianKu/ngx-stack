e3a7f93d6303a21d6487b9a05004ccc193c56b15
Damian Kuriata <damianisko7.dk@gmail.com>
2023-04-23 19:57:10 +0100

    Update JSDocs and types for cacheRequest method

--- projects/http-cache/src/lib/http-cache.ts
+++ projects/http-cache/src/lib/http-cache.ts
@@ -1,12 +1,17 @@
 import { HttpContext, HttpContextToken } from "@angular/common/http";
 
 /**
- * 
+ * {@link HttpContextToken} used to set {@link HttpRequest} as cachable
  */
 export const HTTP_CACHE_TOKEN = new HttpContextToken<boolean>(() => false);
 
 /**
+ * Return a {@link HttpContext} that will cache {@link HttpRequest}
  * 
- * @returns 
+ * Usage
+ * ```ts
+ * http.get(URL, {context: cacheRequest()})
+ * ```
+ * @returns {HttpContext}
  */
-export const cacheRequest = () => new HttpContext().set(HTTP_CACHE_TOKEN, true);
+export const cacheRequest = (): HttpContext => new HttpContext().set(HTTP_CACHE_TOKEN, true);
